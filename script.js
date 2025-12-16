5487dd6256f13c6bd1fb9a3f9f9447272e9dd874
gmeilkom2-spec <gmeilkom2@gmail.com>
2025-12-16 16:51:46 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Заглушка для GitHub Pages (без backend)
+function register() {
+alert('Регистрация доступна в версии с сервером');
+}
+
+
+function login() {
+alert('Вход доступен в версии с сервером');
+}
